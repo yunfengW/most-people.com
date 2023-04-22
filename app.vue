a411874ad450e52cfc2b8cd363eae3326166f317
大海 <mp@sea.team>
2023-04-23 00:24:56 +0800

    remove api key

--- app.vue
+++ app.vue
@@ -1,68 +1,91 @@
 <template>
   <div id="app">
-    <div class="ip-address">
-      <h4>
-        IP 地址：{{ form.continent }} {{ form.country }} {{ form.regionName }}
-        {{ form.city }}
-      </h4>
+    <el-form @submit.prevent ref="formElement" :model="form">
+      <div class="ip-address">
+        <h4>
+          IP 地址：{{ form.continent }} {{ form.country }} {{ form.regionName }}
+          {{ form.city }}
+        </h4>
 
-      <el-button
-        v-if="!form.countryCode"
-        type="primary"
-        @click="getLocation"
-        :loading="form.locationLoading"
-        link
-      >
-        获取 IP 地址
-      </el-button>
-      <el-link
-        style="margin-left: auto"
-        type="info"
-        href="http://ip111.cn/"
-        target="_blank"
-      >
-        自助查询 IP 地址
-      </el-link>
-    </div>
-
-    <div class="api-key">
-      <h4>
-        OpenAI API 密钥
-        <el-link
-          href="https://platform.openai.com/account/api-keys"
-          target="_blank"
-          type="info"
+        <el-button
+          v-if="!form.countryCode"
+          type="primary"
+          @click="getLocation"
+          :loading="form.locationLoading"
+          link
+          disabled
         >
-          https://platform.openai.com/account/api-keys
+          获取 IP 地址
+        </el-button>
+        <el-link
+          style="margin-left: auto"
+          type="info"
+          href="http://ip111.cn/"
+          target="_blank"
+        >
+          自助查询 IP 地址
         </el-link>
-      </h4>
-      <el-input v-model="form.apiKey" clearable show-password />
-    </div>
-    <div class="question">
-      <h4>发送内容</h4>
-      <div class="message-box">
-        <el-input
-          v-model="form.message"
-          :autosize="{ minRows: 2 }"
-          type="textarea"
-          resize="none"
-        />
+      </div>
+      <div class="api-key">
+        <h4>
+          OpenAI API 密钥
+          <el-link
+            href="https://platform.openai.com/account/api-keys"
+            target="_blank"
+            type="info"
+          >
+            https://platform.openai.com/account/api-keys
+          </el-link>
+        </h4>
+        <el-form-item
+          :rules="[
+            {
+              required: true,
+              message: '请输入 OpenAI API 密钥',
+              trigger: 'blur',
+            },
+          ]"
+          prop="apiKey"
+        >
+          <el-input v-model="form.apiKey" clearable show-password />
+        </el-form-item>
+      </div>
+      <div class="question">
+        <h4>发送内容</h4>
+        <el-form-item
+          :rules="[
+            {
+              required: true,
+              message: '请输入要发送的内容',
+              trigger: 'blur',
+            },
+          ]"
+          prop="message"
+        >
+          <el-input
+            v-model="form.message"
+            :autosize="{ minRows: 2 }"
+            type="textarea"
+            resize="none"
+          />
+        </el-form-item>
         <el-button type="primary" @click="submit" :loading="form.submitLoading">
           提交
         </el-button>
       </div>
-    </div>
-    <div class="reasons">
-      <br />
-      <div class="reason" v-for="reason in form.reasons">
-        {{ reason.message.content }}
+      <div class="reasons">
+        <br />
+        <div class="reason" v-for="reason in form.reasons">
+          {{ reason.message.content }}
+        </div>
       </div>
-    </div>
+    </el-form>
   </div>
 </template>
 
 <script setup lang="ts">
 import axios from "axios";
+import { FormInstance } from "element-plus";
 
 interface Reason {
   message: {
@@ -79,35 +102,35 @@ const form = reactive({
   regionName: "",
   locationLoading: false,
   // OpenAI
-  apiKey: "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",
+  apiKey: "",
   message: "你好",
   submitLoading: false,
   reasons: [] as Reason[],
 });
 
-const submit = async () => {
-  if (!form.countryCode) {
-    ElMessage.warning("请先获取 IP 地址");
-    return;
-  }
-  if (["HK", "CN"].includes(form.countryCode)) {
-    ElMessage.info("OpenAI 暂不支持 中国大陆和香港");
-    return;
-  }
-  if (!form.apiKey) {
-    return;
-  }
-  if (!form.message) {
-    return;
-  }
-  form.submitLoading = true;
-  try {
-    const res = await chatWithGPT3(form.apiKey, form.message);
-    form.reasons = res.data.choices;
-  } catch (error: any) {
-    ElMessageBox.alert(error.response.data.error.message);
-  }
-  form.submitLoading = false;
+const formElement = ref<FormInstance>();
+const submit = () => {
+  if (!formElement.value) return;
+  formElement.value.validate(async (ok) => {
+    if (ok) {
+      // if (!form.countryCode) {
+      //   ElMessage.warning("请先获取 IP 地址");
+      //   return;
+      // }
+      // if (["HK", "CN"].includes(form.countryCode)) {
+      //   ElMessage.info("OpenAI 暂不支持 中国大陆和香港");
+      //   return;
+      // }
+      form.submitLoading = true;
+      try {
+        const res = await chatWithGPT3(form.apiKey, form.message);
+        form.reasons = res.data.choices;
+      } catch (error: any) {
+        ElMessageBox.alert(error.response?.data?.error?.message || "未知错误");
+      }
+      form.submitLoading = false;
+    }
+  });
 };
 
 const getLocation = () => {
@@ -164,12 +187,8 @@ const chatWithGPT3 = (apiKey: string, message: string) => {
   }
   // .api-key {}
   .question {
-    .message-box {
-      display: flex;
-      align-items: flex-end;
-      .el-button {
-        margin-left: 10px;
-      }
+    .el-button {
+      width: 100%;
     }
   }
 }
