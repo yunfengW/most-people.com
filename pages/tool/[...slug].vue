bf6a7d4caea1f4a52b5f539bff6ac48cf1fd448e
大海 <mp@sea.team>
2023-05-03 15:00:30 +0800

    哈吉米

---
+++ pages/tool/[...slug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const { data } = await useAsyncData('get-document', () => queryContent(route.path).findOne())
+</script>
+
+<template>
+  <div>
+    <mp-header :content="data?.title" />
+    <ContentRenderer v-if="data" :value="data" />
+  </div>
+</template>
--- pages/tool/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div id="page-tools">
-    <mp-header content="万能工具箱" />
-  </div>
-</template>
