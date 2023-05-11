5d7d9b14cba364bac7f9df5d0607064b86cf4bed
大海 <mp@sea.team>
2023-05-11 08:41:24 +0800

    购物

--- composables/useIndex.ts
+++ composables/useIndex.ts
@@ -17,7 +17,8 @@ export const useIndex = () => {
     }
     if (url.includes('「most-people」')) {
       const keyword = encodeURIComponent(userStore.message || form.placeholder)
-      return url.replace('「most-people」', keyword)
+      url = url.replace('「most-people」', keyword)
+      return url.replace(/「[^」]*」/g, '')
     }
     const urlObject = new URL(url)
     const keyword = userStore.message || form.placeholder
@@ -26,7 +27,27 @@ export const useIndex = () => {
   }
   const send = () => {
     const url = formatURL(userStore.tool.url)
-    window.open(url)
+    if (userStore.tool.url.includes('「mobile」')) {
+      const width = 430
+      const height = 844
+      // 计算窗口的位置
+      const screenWidth =
+        window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+      const screenHeight =
+        window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+      const left = screenWidth - width - screenWidth * 0.2
+      const top = (screenHeight - height) / 2
+      // 设置窗口的大小和位置
+      const features = `width=${width},height=${height},left=${left},top=${top}`
+      // 打开一个新的浏览器窗口
+      const newWindow = window.open(url, '_blank', features)
+      // 如果浏览器阻止了弹出窗口，则直接打开
+      if (!newWindow || newWindow.closed || typeof newWindow.closed === 'undefined') {
+        window.open(url)
+      }
+    } else {
+      window.open(url)
+    }
   }
   const microphone = () => {
     ElMessage.info('语音输入 正在开发')
