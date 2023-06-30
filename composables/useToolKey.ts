4c9038688794a2a90d1b4a71c67089aaff153d06
大海 <mp@sea.team>
2023-07-01 03:47:38 +0800

    editor

---
+++ composables/useToolKey.ts
@@ -0,0 +1,109 @@
+import axios from 'axios'
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import rehypeRaw from 'rehype-raw'
+import rehypeStringify from 'rehype-stringify'
+import remarkRehype from 'remark-rehype'
+import DOMPurify from 'dompurify'
+import tools from '~/assets/json/tools.json'
+
+export const useToolKey = () => {
+  // https://github.com/remarkjs/remark
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeStringify).process
+
+  const html = ref('')
+  const inited = ref(false)
+  const markdownElement = ref<HTMLDivElement>()
+  const route = useRoute()
+  const toolKey = route.params.toolKey
+
+  const toolName = computed(() => {
+    const tool = tools[toolKey as 'Bing']
+    if (tool) {
+      return tool.zh
+    }
+    return toolKey
+  })
+
+  const render = async (md: string) => {
+    const file = await processor(md)
+    const clean = DOMPurify.sanitize(file.toString(), { ADD_TAGS: ['mp-mi'] })
+    html.value = clean
+    // mp-mi
+    nextTick(() => {
+      if (!markdownElement.value) return
+      const miList = markdownElement.value.querySelectorAll('mp-mi') as unknown as HTMLDivElement[]
+      for (const mi of miList) {
+        mi.addEventListener('click', async (event: any) => {
+          const tagName = event?.target?.tagName
+          if (tagName === 'A') {
+            const input = event.target.previousSibling
+            if (input.tagName === 'INPUT') {
+              const password = input.value as string
+              const text = mi.querySelector('span')?.innerText || ''
+              const { key } = await mp.key('most-people', password)
+              const decrypted = await mp.decrypt(text, key)
+              if (decrypted) {
+                mi.innerHTML = `<div>${decrypted.replaceAll('\n', '<br />')}</div>`
+              } else {
+                mp.error('密码错误')
+              }
+            }
+          }
+        })
+        mi.innerHTML = `<span>${mi.innerText}</span><input placeholder="输入密码" /><a>解密</a>`
+      }
+    })
+  }
+
+  const markdown = ref('')
+  const init = () => {
+    axios({
+      url: `https://cdn.most-people.cn/tool/${toolKey}.md`,
+    })
+      .then((res) => {
+        const md = res.data as string
+        if (md) {
+          markdown.value = md
+          render(md)
+        }
+      })
+      .catch((err) => {
+        // mp.error(err.message)
+      })
+      .finally(() => {
+        inited.value = true
+      })
+  }
+
+  if (process.client) {
+    init()
+  }
+
+  const editorElement = ref<HTMLDivElement>()
+  onMounted(async () => {
+    const { editor } = await import('monaco-editor')
+    if (!editorElement.value) {
+      return
+    }
+    const markdownEditor = editor.create(editorElement.value, {
+      value: markdown.value,
+      language: 'markdown',
+      tabSize: 2,
+      minimap: {
+        enabled: false,
+      },
+    })
+  })
+
+  return {
+    inited,
+    html,
+    toolName,
+    editorElement,
+  }
+}
--- pages/tool/[toolKey].vue
+++ pages/tool/[toolKey].vue
@@ -10,161 +10,90 @@
       </template>
     </mp-header>
 
-    <div v-show="html" ref="markdownElement" class="markdown-box" v-html="html"></div>
-    <div v-if="inited && !html" class="empty">
+    <div class="markdown-box" ref="markdownElement" v-html="html"></div>
+
+    <div class="markdown-empty" v-if="inited && !html">
       <h4>抱歉，暂时还没有「{{ toolName }}」的使用指南</h4>
       <div>如果你有兴趣的话，加入我们吧</div>
       <br />
       <el-image class="join-us" src="/img/join-us.jpg" />
     </div>
+
+    <div class="markdown-editor" ref="editorElement"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import axios from 'axios'
-import { unified } from 'unified'
-import remarkParse from 'remark-parse'
-import rehypeRaw from 'rehype-raw'
-import rehypeStringify from 'rehype-stringify'
-import remarkRehype from 'remark-rehype'
-import DOMPurify from 'dompurify'
-import { useUserStore } from '~/stores/user'
-import tools from '~/assets/json/tools.json'
+import { useToolKey } from '~/composables/useToolKey'
 
-// https://github.com/remarkjs/remark
-const processor = unified()
-  .use(remarkParse)
-  .use(remarkRehype, { allowDangerousHtml: true })
-  .use(rehypeRaw)
-  .use(rehypeStringify).process
-
-const html = ref('')
-const inited = ref(false)
-const markdownElement = ref<HTMLDivElement>()
-const userStore = useUserStore()
-
-const render = async (md: string) => {
-  const file = await processor(md)
-  const clean = DOMPurify.sanitize(file.toString(), { ADD_TAGS: ['mp-mi'] })
-  html.value = clean
-  // mp-mi
-  nextTick(() => {
-    if (!markdownElement.value) return
-    const miList = markdownElement.value.querySelectorAll('mp-mi') as unknown as HTMLDivElement[]
-    for (const mi of miList) {
-      mi.addEventListener('click', async (event: any) => {
-        const tagName = event?.target?.tagName
-        if (tagName === 'A') {
-          const input = event.target.previousSibling
-          if (input.tagName === 'INPUT') {
-            const password = input.value as string
-            const text = mi.querySelector('span')?.innerText || ''
-            const { key } = await mp.key('most-people', password)
-            const decrypted = await mp.decrypt(text, key)
-            if (decrypted) {
-              mi.innerHTML = `<div>${decrypted.replaceAll('\n', '<br />')}</div>`
-            } else {
-              mp.error('密码错误')
-            }
-          }
-        }
-      })
-      mi.innerHTML = `<span>${mi.innerText}</span><input placeholder="输入密码" /><a>解密</a>`
-    }
-  })
-}
-
-const route = useRoute()
-const toolKey = route.params.toolKey
-const toolName = computed(() => {
-  const tool = tools[toolKey as 'Bing']
-  if (tool) {
-    return tool.zh
-  }
-  return toolKey
-})
-const init = () => {
-  axios({
-    url: `https://cdn.most-people.cn/tool/${toolKey}.md`,
-  })
-    .then((res) => {
-      const md = res.data as string
-      if (md) {
-        render(md)
-      }
-    })
-    .catch((err) => {
-      // mp.error(err.message)
-    })
-    .finally(() => {
-      inited.value = true
-    })
-}
-if (process.client) {
-  init()
-}
+const { inited, html, toolName, editorElement } = useToolKey()
 </script>
 
 <style lang="scss">
 #page-tool {
   .markdown-box {
     word-break: break-word;
+    width: 100%;
+
+    mp-mi {
+      display: flex;
+      flex-direction: column;
+      position: relative;
+
+      > div {
+        color: #000;
+      }
+      > span {
+        color: #f1f1f1;
+      }
+
+      > div,
+      > span {
+        width: 100%;
+        padding: 16px;
+        background-color: white;
+        border-radius: 20px;
+        font-size: 16px;
+        line-height: 24px;
+      }
+      > input {
+        width: 100%;
+        padding: 0 16px;
+        height: 56px;
+        border-radius: 20px;
+        outline: 0;
+        border: 0;
+        font-size: 20px;
+        margin: 10px 0;
+      }
+      > a {
+        position: absolute;
+        right: 16px;
+        bottom: 20px;
+        display: inline-flex;
+        align-items: center;
+        height: 36px;
+        background-color: rgb(29, 155, 240);
+        color: white;
+        margin-left: 16px;
+        padding: 0 16px;
+        border-radius: 20px;
+
+        &:hover {
+          background-color: rgb(23, 124, 192);
+        }
+      }
+    }
   }
-  .empty {
+  .markdown-empty {
     text-align: center;
     .join-us {
       max-width: 375px;
     }
   }
-
-  mp-mi {
-    display: flex;
-    flex-direction: column;
-    position: relative;
-
-    > div {
-      color: #000;
-    }
-    > span {
-      color: #f1f1f1;
-    }
-
-    > div,
-    > span {
-      width: 100%;
-      padding: 16px;
-      background-color: white;
-      border-radius: 20px;
-      font-size: 16px;
-      line-height: 24px;
-    }
-    > input {
-      width: 100%;
-      padding: 0 16px;
-      height: 56px;
-      border-radius: 20px;
-      outline: 0;
-      border: 0;
-      font-size: 20px;
-      margin: 10px 0;
-    }
-    > a {
-      position: absolute;
-      right: 16px;
-      bottom: 20px;
-      display: inline-flex;
-      align-items: center;
-      height: 36px;
-      background-color: rgb(29, 155, 240);
-      color: white;
-      margin-left: 16px;
-      padding: 0 16px;
-      border-radius: 20px;
-
-      &:hover {
-        background-color: rgb(23, 124, 192);
-      }
-    }
+  .markdown-editor {
+    width: 100%;
+    height: 800px;
   }
 }
 </style>
