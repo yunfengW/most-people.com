1d3ad86a98051d535c17e77afedb4096ba6c1b79
大海 <iwangyang@qq.com>
2023-10-15 04:17:20 +0800

    Create publish.yml
    env
    
    Update publish.yml
    
    Update publish.yml
    
    Update publish.yml

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,23 +1,19 @@
+# https://help.aliyun.com/document_detail/120075.html
+
 npm run generate
 
-endpoint=https://oss-rg-china-mainland.aliyuncs.com
-# accessKeyID=
-# accessKeySecret=
-
-Bucket=most-people-com
+Endpoint=$ENDPOINT
+AccessKeyID=$ACCESS_KEY_ID
+AccessKeySecret=$ACCESS_KEY_SECRET
+Bucket=$BUCKET
 
 # 查询所有文件
-# ./bin/ossutil64 -e $endpoint -i $accessKeyID -k $accessKeySecret ls oss://$Bucket/
+# ./bin/ossutil64 -e $Endpoint -i $AccessKeyID -k $AccessKeySecret ls oss://$Bucket/
 
 # 删除所有数据
 ./bin/ossutil64 rm -r -f oss://$Bucket/
 
 # 上传本地文件夹
-local_folder_name=.output/public
-remote_folder_name=
-# ./bin/ossutil64 -e $endpoint -i $accessKeyID -k $accessKeySecret cp -r $local_folder_name oss://$Bucket/$remote_folder_name
-
-
-# https://help.aliyun.com/document_detail/120075.html
-# 使用本地的 .ossutilconfig
-./bin/ossutil64 cp -r $local_folder_name oss://$Bucket/$remote_folder_name
+LocalFolder=.output/public
+RemoteFolder=
+./bin/ossutil64 -e $Endpoint -i $AccessKeyID -k $AccessKeySecret cp -r $LocalFolder oss://$Bucket/$RemoteFolder
