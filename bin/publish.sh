c718ec602dfe461c4d7c7c81150969d86838b0ff
大海 <iwangyang@qq.com>
2023-10-19 20:24:08 +0800

    Update publish.sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,18 +2,29 @@
 
 npm run generate
 
-Endpoint=$ENDPOINT
+Endpoint=https://oss-rg-china-mainland.aliyuncs.com
 AccessKeyID=$ACCESS_KEY_ID
 AccessKeySecret=$ACCESS_KEY_SECRET
-Bucket=$BUCKET
+Bucket=most-people-com
 
 # 查询所有文件
 # ./bin/ossutil64 -e $Endpoint -i $AccessKeyID -k $AccessKeySecret ls oss://$Bucket/
 
-# 删除所有数据
-./bin/ossutil64 rm -r -f oss://$Bucket/
-
 # 上传本地文件夹
 LocalFolder=.output/public
 RemoteFolder=
-./bin/ossutil64 -e $Endpoint -i $AccessKeyID -k $AccessKeySecret cp -r $LocalFolder oss://$Bucket/$RemoteFolder
+
+if [ "$AccessKeyID" = "" ] && [ "$AccessKeySecret" = "" ]; then
+    echo local
+    # 使用本地的 .ossutilconfig
+    # 删除所有数据
+    ./bin/ossutil64 rm -r -f oss://$Bucket/
+    # 重新上传
+    ./bin/ossutil64 cp -r $LocalFolder oss://$Bucket/$RemoteFolder
+else
+    echo github actions
+    ./bin/ossutil64 -e $Endpoint -i $AccessKeyID -k $AccessKeySecret rm -r -f oss://$Bucket/
+    ./bin/ossutil64 -e $Endpoint -i $AccessKeyID -k $AccessKeySecret cp -r $LocalFolder oss://$Bucket/$RemoteFolder
+fi
+
+echo https://most-people.cn/
