565099b4242d7fd0f9c4513e2de64b2cb47cd92b
大海 <iwangyang@qq.com>
2024-02-04 15:01:45 +0800

    send

--- pages/chat/[person_id].vue
+++ pages/chat/[person_id].vue
@@ -3,6 +3,13 @@
     <mp-header title="单线联系" />
     {{ userStore.user?.id }} to
     {{ route.params.person_id }}
+
+    <div class="send-box">
+      <div class="box">
+        <el-input v-model="form.message" type="textarea" :autosize="{ minRows: 2 }" resize="none" />
+        <el-button type="primary" @click="send" :disabled="!form.message">发送</el-button>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -10,7 +17,24 @@ import api from '~/utils/api'
 
 const userStore = useUserStore()
 const route = useRoute()
-onMounted(() => {
+const form = reactive({
+  message: '',
+})
+
+const send = async () => {
+  const res = await api({
+    method: 'put',
+    url: `/chat/${route.params.person_id}`,
+    data: {
+      message: form.message,
+    },
+  })
+  if (!res.data) {
+    console.error(res)
+  }
+}
+
+const init = () => {
   // 创建一个新的EventSource实例，连接到你的SSE端点
   const url = new URL(api.getUri())
   url.pathname = '/chat/connect'
@@ -23,13 +47,38 @@ onMounted(() => {
   })
   sse.addEventListener('message', (event) => {
     // 解析收到的数据
-    const message = event.data
-    console.log('🌊', message)
+    const data = event.data as string
+    try {
+      console.log('🌊', JSON.parse(data))
+    } catch (error) {
+      console.error(error)
+    }
   })
   sse.addEventListener('error', (error) => {
     console.error('EventSource failed:', error)
     // 解析收到的数据
     sse.close()
   })
+}
+
+onMounted(() => {
+  init()
 })
 </script>
+
+<style lang="scss">
+#page-chat-person {
+  .send-box {
+    position: fixed;
+    bottom: 40px;
+    left: 0;
+    right: 0;
+    .box {
+      margin: 0 auto;
+      width: 61.8%;
+      display: flex;
+      gap: 8px;
+    }
+  }
+}
+</style>
